b0b0d11ebcb02461e8e13e0a003c4d0293547832
sairammamidi576 <mamidisairam576@gmail.com>
2024-08-23 12:05:08 +0530

    QuizProject

---
+++ git/src/Quiz/quiz.js
@@ -0,0 +1,66 @@
+export const questions = [
+    {
+        question : "Inside which HTML element do we put the Javascript?",
+            option1 : "js", 
+            option2 : "javascript", 
+            option3 : "script", 
+            option4 : "scripting",
+            ans : 3
+    },
+    {
+        question : "What is the correct Javascript Syntax to change the content of the HTML element below?",
+            option1 : "document.getElement('p').innerHTML = 'Hello World!';", 
+            option2 : "#demo.innerHTML = 'Hello World!';", 
+            option3 : "document.getElementById('p').innerHTML = 'Hello World!';", 
+            option4 : "document.getElementBYName('p').innerHTML = 'Hello World!';",
+            ans : 3
+    },
+    {
+        question : "Where is the correct place to insert a JavaScript?",
+            option1 : "The footer", 
+            option2 : "The body section", 
+            option3 : "The head section", 
+            option4 : "Both the head and body section",
+            ans : 4
+    },
+    {
+        question : "How do you create a function in JavaScript?",
+            option1 : "function = myFunction(){}", 
+            option2 : "function.myFunction(){}", 
+            option3 : "function = myFunction(){}", 
+            option4 : "function myFunction()={}",
+            ans : 4
+    },
+    {
+        question : "How to write an If statement in JavaScript?",
+            option1 : "if i== 5 then", 
+            option2 : "if(i == 5)",
+            option3 : "if i = 5 then", 
+            option4 : "if i = 5", 
+            ans : 2
+    },
+    {
+        question : "How do we add a comment in a JavaScrript?",
+            option1 : "!--This is a comment-->", 
+            option2 : "//This is a comment", 
+            option3 : "'This is a comment'", 
+            option4 : "{This is a comment}",
+            ans : 2
+    },
+    {
+        question : "What is the correct way to write a JavaScript array?",
+            option1 : "var colors = ['red','green','blue']", 
+            option2 : "var colors = 1 = ('red'),colors = 2 = ('green'),colors = 3 = ('blue'),", 
+            option3 : "var colors = 'red','green','blue'", 
+            option4 : "var colors = (1:'red',2:'green',3:'blue')",
+            ans : 1
+    },
+    {
+        question : "How do you declare a JavaScript variable?",
+            option1 : "v carName;", 
+            option2 : "var carName;", 
+            option3 : "variable carName", 
+            option4 : "None",
+            ans : 2
+    }
+]
